e983a7773fe02b89fde9aea17508d86b8b870ac3
Игорь Никонов <u1e_e1e@mail.ru>
2021-10-22 01:57:45 +0300

    добавлена управляемая форма с инпутом value которого записывается из state, а onChange меняет state

--- src/components/playground/Playground.js
+++ src/components/playground/Playground.js
@@ -1,7 +1,38 @@
 import React, {Component} from 'react';
 
+class ManagedForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+    this.nameInputChangeHandler = this.nameInputChangeHandler.bind(this);
+    this.formSubmitHandler = this.formSubmitHandler.bind(this);
+  }
+
+  nameInputChangeHandler(event) {
+    this.setState({value: event.target.value})
+  }
+
+  formSubmitHandler(event) {
+    event.preventDefault();
+    alert(this.state.value);
+    this.setState({value: ''});
+  }
+
+  render() {
+    return(
+        <form onSubmit={this.formSubmitHandler}>
+          <label>
+            Имя:
+            <input type="text" value={this.state.value} onChange={this.nameInputChangeHandler} />
+          </label>
+          <input type="submit" value="Отправить" />
+        </form>
+    );
+  }
+}
+
 export class Playground extends Component {
   render() {
-    return <span>Welcome to Playground!</span>;
+    return <ManagedForm/>;
   }
 }
