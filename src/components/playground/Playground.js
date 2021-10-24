e0da044cb3ca0d9da39ae581372d304e9ae5164f
Игорь Никонов <u1e_e1e@mail.ru>
2021-10-24 20:51:16 +0300

    функциональный компонет с вводом температур с ввариацией отображения + функциональный компонент вывода результата вводов

--- src/components/playground/Playground.js
+++ src/components/playground/Playground.js
@@ -1,7 +1,66 @@
 import React, {Component} from 'react';
 
+function WaterBoilingResult(props) {
+  const isBoilingTemperature = props.temperature > 99;
+  const resultMessage = isBoilingTemperature ? 'Вода кипит' : 'Вода не кипит';
+  return (
+      <>
+        <br/>
+        <span>{resultMessage}</span>
+      </>
+  );
+
+}
+
+function TemperatureInputBlock(props) {
+  const {temperature, temperatureInputChangeHandler, scaleName} = props;
+  const scaleNames = {
+    c: 'Цельсиях',
+    f: 'Фаренгейтах',
+  };
+  return (
+      <>
+        <legend>Введите температуру в {scaleNames[scaleName]}:</legend>
+        <input value={temperature} onChange={temperatureInputChangeHandler}/>
+        <br/>
+      </>
+  );
+}
+
+class Calculator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {temperature: ''};
+    this.temperatureInputChangeHandler = this.temperatureInputChangeHandler.bind(this);
+  }
+
+  temperatureInputChangeHandler(event) {
+    const value = event.target.value;
+    this.setState({temperature: value});
+  }
+
+  render() {
+    const {temperature} = this.state;
+    return(
+        <>
+          <TemperatureInputBlock
+              scaleName='c'
+              temperature={temperature}
+              temperatureInputChangeHandler={this.temperatureInputChangeHandler}
+          />
+          <TemperatureInputBlock
+              scaleName='f'
+              temperature={temperature}
+              temperatureInputChangeHandler={this.temperatureInputChangeHandler}
+          />
+          <WaterBoilingResult temperature={temperature}/>
+        </>
+    );
+  }
+}
+
 export class Playground extends Component {
   render() {
-    return <span>Welcome to Playground!</span>;
+    return <Calculator/>;
   }
 }
